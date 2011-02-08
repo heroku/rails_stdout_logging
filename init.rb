8fd00304675764bae737c290db84008fdac16a5c
David Dollar <ddollar@gmail.com>
2011-02-07 19:38:54 -0500

    refactor rails 2 support

--- init.rb
+++ init.rb
@@ -1,7 +1,24 @@
-logger = Logger.new(STDOUT)
-logger.level = Logger::INFO
-
 case Rails::VERSION::MAJOR
-  when 3 then Rails.logger = Rails.application.config.logger = logger
-  when 2 then Object.const_set "RAILS_DEFAULT_LOGGER", logger
+
+when 3 then
+  logger = Logger.new(STDOUT)
+  logger.level = Logger::INFO
+  Rails.logger = Rails.application.config.logger = logger
+
+when 2 then
+  # redefine Rails.logger
+  def Rails.logger
+    @@logger ||= begin
+      logger = Logger.new(STDOUT)
+      logger.level = Logger::INFO
+      logger
+    end
+  end
+
+  # borrowed from Rails::Initializer#initialize_framework_logging
+  ([ :active_record, :action_controller, :action_maler ] & Rails.configuration.frameworks).each do |framework|
+    framework.to_s.camelize.constantize.const_get("Base").logger = Rails.logger
+  end
+  ActiveSupport::Dependencies.logger = Rails.logger
+  Rails.cache.logger = Rails.logger
 end
