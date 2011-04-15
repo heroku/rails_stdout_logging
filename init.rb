788f2adf0065ac14e76a624fe88cd3423a1b00d2
Caio Chassot <dev@caiochassot.com>
2011-04-16 05:31:24 +0800

    DRY and stuff

--- init.rb
+++ init.rb
@@ -1,20 +1,20 @@
 STDOUT.sync = true
 
+def Rails.heroku_stdout_logger
+  logger = Logger.new(STDOUT)
+  logger.level = Logger.const_get((%w[DEBUG INFO WARN ERROR FATAL UNKNOWN] & [ENV['LOG_LEVEL'].to_s.upcase, "INFO"]).compact.first)
+  logger
+end
+
 case Rails::VERSION::MAJOR
 
 when 3 then
-  logger = Logger.new(STDOUT)
-  logger.level = Logger.const_get((%w[DEBUG INFO WARN ERROR FATAL UNKNOWN] & [ENV['LOG_LEVEL'].to_s.upcase, "INFO"]).compact.first)
-  Rails.logger = Rails.application.config.logger = logger
+  Rails.logger = Rails.application.config.logger = Rails.heroku_stdout_logger
 
 when 2 then
   # redefine Rails.logger
   def Rails.logger
-    @@logger ||= begin
-      logger = Logger.new(STDOUT)
-      logger.level = Logger.const_get((%w[DEBUG INFO WARN ERROR FATAL UNKNOWN] & [ENV['LOG_LEVEL'].to_s.upcase, "INFO"]).compact.first)
-      logger
-    end
+    @@logger ||= Rails.heroku_stdout_logger
   end
 
   # borrowed from Rails::Initializer#initialize_framework_logging
