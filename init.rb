a943c07c317579659593ba4d750535a24c6471d5
Caio Chassot <dev@caiochassot.com>
2011-04-16 05:38:04 +0800

    Style police at your service

--- init.rb
+++ init.rb
@@ -7,19 +7,15 @@ def Rails.heroku_stdout_logger
 end
 
 case Rails::VERSION::MAJOR
-
-when 3 then
-  Rails.logger = Rails.application.config.logger = Rails.heroku_stdout_logger
-
-when 2 then
-  # redefine Rails.logger
-  def Rails.logger
-    @@logger ||= Rails.heroku_stdout_logger
-  end
-
-  # borrowed from Rails::Initializer#initialize_framework_logging
-  [ActiveSupport::Dependencies, Rails.cache].concat(
-    ([:active_record, :action_controller, :action_mailer] & Rails.configuration.frameworks)\
-      .map { |framework| framework.to_s.camelize.constantize.const_get("Base") }
-  ).each { |k| k.logger = Rails.logger }
+  when 3 then Rails.logger = Rails.application.config.logger = Rails.heroku_stdout_logger
+  when 2 then
+    # redefine Rails.logger
+    def Rails.logger
+      @@logger ||= Rails.heroku_stdout_logger
+    end
+    # borrowed from Rails::Initializer#initialize_framework_logging
+    [ActiveSupport::Dependencies, Rails.cache].concat(
+      ([:active_record, :action_controller, :action_mailer] & Rails.configuration.frameworks)\
+        .map { |framework| framework.to_s.camelize.constantize.const_get("Base") }
+    ).each { |k| k.logger = Rails.logger }
 end
