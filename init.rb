b1b132f4c1c2acfa7cf41b6a94ad829b290b71a3
Caio Chassot <dev@caiochassot.com>
2011-04-16 05:44:43 +0800

    fix order of arrays so INFO doesn't wrongly win in some cases

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ STDOUT.sync = true
 
 def Rails.heroku_stdout_logger
   logger = Logger.new(STDOUT)
-  logger.level = Logger.const_get((%w[DEBUG INFO WARN ERROR FATAL UNKNOWN] & [ENV['LOG_LEVEL'].to_s.upcase, "INFO"]).compact.first)
+  logger.level = Logger.const_get(([ENV['LOG_LEVEL'].to_s.upcase, "INFO"] & %w[DEBUG INFO WARN ERROR FATAL UNKNOWN]).compact.first)
   logger
 end
 
